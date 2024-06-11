c7e13163b14a0a73895e3204cbfef579d1126272
Sergiy Dybskiy <s@serg.tech>
2024-06-11 12:20:36 -0400

    feat: Form submission

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -126,7 +126,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('tailwindcss-aria-attributes')],
 } satisfies Config;
 
 export default config;

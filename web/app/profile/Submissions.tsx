fd14969fef5f31f7349b1fe6c45208d35864c415
Sergiy Dybskiy <s@serg.tech>
2024-06-12 09:49:14 -0400

    loading

--- web/app/profile/Submissions.tsx
+++ web/app/profile/Submissions.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSubmissions } from '@/hooks/useSubmissions';
-import { loadingMarkup } from 'app/home/FeaturedRepos';
+
 import Submission from './Submission';
 
 function PullRequestList() {
@@ -11,7 +11,11 @@ function PullRequestList() {
 
   return submissions && submissions.length > 0 ? (
     <div className="mt-10 grid grid-cols-1 gap-6 md:mt-12 md:grid-cols-2 md:gap-8 lg:mt-14 lg:grid-cols-3 lg:gap-10 xl:grid-cols-4">
-      {isLoading ? loadingMarkup : submissions?.map((pr) => <Submission key={pr.id} pr={pr} />)}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        submissions?.map((pr) => <Submission key={pr.id} pr={pr} />)
+      )}
     </div>
   ) : (
     <div>No submissions yet, good luck with your first one!</div>

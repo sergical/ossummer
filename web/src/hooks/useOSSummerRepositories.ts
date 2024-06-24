6a5589e3e67d75bd55e5c67fdb03451459de62c2
Sergiy Dybskiy <s@serg.tech>
2024-06-23 22:55:59 -0400

    default url

--- web/src/hooks/useOSSummerRepositories.ts
+++ web/src/hooks/useOSSummerRepositories.ts
@@ -1,11 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
+import { DEFAULT_URL } from '@/constants';
 import { Repository } from '@/types/github';
 
 export function useOSSummerRepositories() {
   return useQuery<Repository[]>({
     queryKey: ['useOSSummerRepositories'],
     queryFn: async () => {
-      const response = await fetch(`/api/github/search`);
+      const response = await fetch(`${DEFAULT_URL}/api/github/search`);
       if (!response.ok) {
         throw new Error('Failed to fetch repositories');
       }
